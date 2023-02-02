800678b64394222b05e35fa6c22a8bf53eb473bd
Roma Malyi <roma.maliy13@gmail.com>
2023-02-02 19:46:42 +0200

    math

--- main.js
+++ main.js
@@ -18,4 +18,23 @@ var num_3 = 5;
 var num_4 = 15;
 
 var res = num_3 - num_4;
-console.log("Minus:" + (num_3 - num_4))
+console.log("Minus:" + (num_3 % num_4))
+
+var num_5 = 5;
+num_5 = num_5 + 7;
+num_5 += 3
+num_5++;
+console.log(num_5)
+
+var str_1 = "12";
+var str_2 = "2";
+console.log("Result : " + (str_1 + str_2))
+
+var str_1 = Number("12");
+var str_2 = Number("2");
+console.log("Result : " + (str_1 + str_2));
+
+console.log("Math : " + Math.PI);
+console.log("Math : " + Math.E);
+console.log("Math : " + Math.min(4,54,47,6,-9,0));
+console.log("Math : " + Math.max(4,54,47,6,-9,0));
