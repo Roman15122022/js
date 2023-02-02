1610d705071c6b835f92b98a115b9985995ca253
Roma Malyi <roma.maliy13@gmail.com>
2023-02-02 19:35:10 +0200

    type of data

---
+++ main.js
@@ -0,0 +1,21 @@
+//console.error("Error 111");
+/*
+var num;
+num = 5;*/
+
+var num = 5;
+num = 7;
+console.log("Variable : " + num);
+
+var number;
+number = 5.5;
+
+var num_1 = 5;
+var num_2 = '5';
+console.log(num_1 + num_2);
+
+var num_3 = 5;
+var num_4 = 15;
+
+var res = num_3 - num_4;
+console.log("Minus:" + (num_3 - num_4))
